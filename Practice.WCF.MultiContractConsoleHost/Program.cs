0027918e388fe6b5c8283fa859981e85521d763f
rajmanioinam <31492859+rajmanioinam@users.noreply.github.com>
2018-01-02 00:39:02 -0700

    Code change

---
+++ Practice.WCF.MultiContractConsoleHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Practice.WCF.MultiContractConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Practice.WCF.MultiContractService.CompanyService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Stated @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
